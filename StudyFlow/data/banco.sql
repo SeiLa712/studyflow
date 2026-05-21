912dc5ff837a3a3a396df257490da555b667ea79
João Pedro <joao42flash2@gmail.com>
2026-05-21 13:54:07 -0300

    acabei o banco

--- StudyFlow/data/banco.sql
+++ StudyFlow/data/banco.sql
@@ -1,1 +1,21 @@
-Meu banco topzinho
+CREATE DATABASE IF NOT EXISTS StudyFlow;
+
+USE StudyFlow;
+
+CREATE TABLE usuarios(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(100),
+    email VARCHAR(100),
+    senha VARCHAR(255),
+    perfil ENUM('administrador', 'estudante')
+);
+
+CREATE TABLE tarefas(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(100),
+	descricao TEXT,
+    prioridade ENUM('baixa', 'media', 'alta') DEFAULT 'media',	
+    id_usuario INT,
+    FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE
+    
+);
