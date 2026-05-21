3d5b943e793cbf252bdd63c09bb907d365b999ea
João Pedro <joao42flash2@gmail.com>
2026-05-21 13:54:22 -0300

    banco

--- StudyFlow/data/banco.sql
+++ StudyFlow/data/banco.sql
@@ -1,21 +1,72 @@
 CREATE DATABASE IF NOT EXISTS StudyFlow;
-
 USE StudyFlow;
 
-CREATE TABLE usuarios(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	nome VARCHAR(100),
-    email VARCHAR(100),
-    senha VARCHAR(255),
-    perfil ENUM('administrador', 'estudante')
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    perfil ENUM('estudante'),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE tarefas(
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	nome VARCHAR(100),
-	descricao TEXT,
-    prioridade ENUM('baixa', 'media', 'alta') DEFAULT 'media',	
-    id_usuario INT,
-    FOREIGN KEY (id_usuario) REFERENCES usuarios(id) ON DELETE CASCADE
-    
+CREATE TABLE grupos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    prioridade ENUM('baixa', 'media', 'alta') DEFAULT 'media',
+    id_usuario INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (id_usuario)
+        REFERENCES usuarios(id)
+        ON DELETE CASCADE
 );
+
+CREATE TABLE tarefas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    prioridade ENUM('baixa', 'media', 'alta') DEFAULT 'media',
+    data_vencimento DATE,
+    concluida BOOLEAN DEFAULT FALSE,
+
+    id_usuario INT NOT NULL,
+    id_grupo INT,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (id_usuario)
+        REFERENCES usuarios(id)
+        ON DELETE CASCADE,
+
+    FOREIGN KEY (id_grupo)
+        REFERENCES grupos(id)
+        ON DELETE SET NULL
+);
+
+CREATE TABLE provas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    descricao TEXT,
+    prioridade ENUM('baixa', 'media', 'alta') DEFAULT 'media',
+
+    data_prova DATE NOT NULL,
+
+    id_usuario INT NOT NULL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (id_usuario)
+        REFERENCES usuarios(id)
+        ON DELETE CASCADE
+);
+
+INSERT INTO usuarios(nome, email, senha, perfil)
+VALUES(
+	'Admin joao', 
+    'joaoadm@gmail.com', 
+    'neymarnelhordetodos', 
+    'estudante'
+);
+
