5c8de01642d97254e18bed26af179faecd8fd5b1
João Pedro <joao42flash2@gmail.com>
2026-07-13 09:39:01 -0300

    Corrige servidor Express para Vercel

--- StudyFlow/server.js
+++ StudyFlow/server.js
@@ -1,4 +1,9 @@
-const express = require("express");
 const app = require("./server/server.js");
+
+const port = Number(process.env.PORT || 3000);
+
+app.listen(port, () => {
+  console.log(`StudyFlow funcionando na porta ${port}`);
+});
 
 module.exports = app;
